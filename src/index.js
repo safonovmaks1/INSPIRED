ee283385906a34d92e02397e6b8ea3feebcd175b
Maks Safonov <safonovmaks1@yandex.ru>
2023-01-15 02:35:03 +0300

    add JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import './index.html'
 import './index.scss'
+
+import { router } from './modules/router'
+
+import { renderHeader } from './modules/render/renderHeader'
+import { mainPage } from './modules/mainPage/mainPage'
+import { womenMainPage } from './modules/mainPage/womenMainPage'
+import { menMainPage } from './modules/mainPage/manMainPage'
+import { renderFooter } from './modules/render/renderFooter'
+
+router.on('*', () => {
+	renderHeader()
+	renderFooter()
+})
+
+router.on('/', () => {
+	mainPage()
+})
+
+router.on('women', () => {
+	womenMainPage()
+})
+
+router.on('men', () => {
+	menMainPage()
+})
+
+// setTimeout(() => {
+// 	router.navigate('men')
+// }, 3000)
+
+// setTimeout(() => {
+// 	router.navigate('women')
+// }, 6000)
+
+router.resolve()
---
+++ src/modules/mainPage/mainPage.js
@@ -0,0 +1,11 @@
+import { renderNavigation } from '../render/renderNavigation'
+import { renderHero } from '../render/renderHero'
+import { renderProducts } from '../render/renderProducts'
+
+export const mainPage = (gender = 'women') => {
+	console.log('gender: ', gender)
+
+	renderNavigation(gender)
+	renderHero(gender)
+	renderProducts(gender)
+}
